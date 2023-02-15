36c001b6034d449ec68bb951a6f3e51847bc0422
crisneames <crisneames@gmail.com>
2023-02-14 18:36:23 -0600

    Add project files.

---
+++ Building.cs
@@ -0,0 +1,62 @@
+﻿namespace UrbanPlanner;
+
+
+public class Building
+{
+    private string _designer = "Cristi";
+    private DateTime _dateConstructed;
+    private string _address;
+    private string _owner;
+
+    public int Stories { get; set; }
+    public double Width { get; set; }
+    public double Depth { get; set; }
+    //public string Address 
+    //{
+    //    get { return _address; }
+    //}
+
+    //public string Designer
+    //{
+    //    get { return _designer; }
+    //}
+
+    //public DateTime DateConstructed
+    //{
+    //    get { return _dateConstructed; }
+    //}
+
+    public double Volume
+    {
+        get { return Width * Depth * (3 * Stories); }
+
+    }
+
+    // constructor
+    public Building(string address)
+    {
+        _address= address;
+    }
+
+    public void Construct()
+    {
+        _dateConstructed= DateTime.Now;
+    }
+
+    public void Purchase(string owner)
+    {
+        _owner= owner;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using UrbanPlanner;
+
+Building FiveOneTwoEigth = new Building("512 8th Avenue");
+FiveOneTwoEigth.Stories = 150;
+FiveOneTwoEigth.Width = 3000;
+FiveOneTwoEigth.Depth = 2000;
+FiveOneTwoEigth.Purchase("Cristi");
+FiveOneTwoEigth.Construct();
+
+Building TwoTwoTwo = new Building("222 7th Street");
+TwoTwoTwo.Stories = 150;
+TwoTwoTwo.Width = 3000;
+TwoTwoTwo.Depth = 2000;
+TwoTwoTwo.Purchase("Cristi");
+TwoTwoTwo.Construct();
+
+List<Building> buildings = new List<Building>();
+buildings.Add(FiveOneTwoEigth);
+buildings.Add(TwoTwoTwo);
+
+foreach(Building building in buildings)
+    {
+    Console.WriteLine($"{building.Address}");
+    Console.WriteLine($"Designed By: {building.Designer}");
+    Console.WriteLine($"Constructed on: {building.DateConstructed}");
+} 
